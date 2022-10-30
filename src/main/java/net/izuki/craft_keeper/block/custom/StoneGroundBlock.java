bfaa522fe5467aa92b9dd9b2c8266edfadfe3aec
IzukiKa <isaacjroberts0505@gmail.com>
2022-10-30 16:24:07 +0000

    Create StoneGroundBlock.java

---
+++ src/main/java/net/izuki/craft_keeper/block/custom/StoneGroundBlock.java
@@ -0,0 +1,29 @@
+package net.izuki.craft_keeper.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class StoneGroundBlock extends Block {
+    public StoneGroundBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if (entity instanceof LivingEntity livingEntity) {
+            livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 200));
+        }
+        super.onSteppedOn(world, pos, state, entity);
+    }
+}
