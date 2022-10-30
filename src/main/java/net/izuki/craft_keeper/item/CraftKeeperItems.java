de76456657e37b7ae940f84ba8750cfb004c7f5b
IzukiKa <isaacjroberts0505@gmail.com>
2022-10-30 16:22:46 +0000

    Create Adder Stone

--- src/main/java/net/izuki/craft_keeper/item/CraftKeeperItems.java
+++ src/main/java/net/izuki/craft_keeper/item/CraftKeeperItems.java
@@ -2,6 +2,7 @@ package net.izuki.craft_keeper.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.izuki.craft_keeper.CraftKeeperMod;
+import net.izuki.craft_keeper.item.custom.*;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -9,6 +10,8 @@ import net.minecraft.util.registry.Registry;
 public class CraftKeeperItems {
 
     // Register valuables
+    public static final Item ADDER_STONE = registerItem("adder_stone",
+            new AdderStoneItem(new FabricItemSettings().group(CraftKeeperItemGroups.VALUABLES).maxCount(999)));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(CraftKeeperMod.MOD_ID, name), item);
---
+++ src/main/java/net/izuki/craft_keeper/item/custom/AdderStoneItem.java
@@ -0,0 +1,35 @@
+package net.izuki.craft_keeper.item.custom;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class AdderStoneItem extends Item {
+    public AdderStoneItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.literal("Valuable").formatted(Formatting.WHITE));
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("A porous smooth stone,").formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("some believe it wards off evil").formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("spirits.").formatted(Formatting.GRAY));
+        } else {
+            tooltip.add(Text.literal("Press SHIFT for more info!").formatted(Formatting.YELLOW));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
