19221683fd4c8f159dac4f5e8e64f2f5dbd5a899
IzukiKa <isaacjroberts0505@gmail.com>
2022-10-30 17:35:49 +0000

    Create Caveling Medal

---
+++ src/main/java/net/izuki/craft_keeper/item/custom/CavelingMedalItem.java
@@ -0,0 +1,32 @@
+package net.izuki.craft_keeper.item.custom;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class CavelingMedalItem extends Item {
+    public CavelingMedalItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.literal("Valuable").formatted(Formatting.WHITE));
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("Minted medallion with a face").formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("relief. Has it been awarded").formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("to someone extraordinary").formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("once?").formatted(Formatting.GRAY));
+        } else {
+            tooltip.add(Text.literal("Press SHIFT for more info!").formatted(Formatting.YELLOW));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
