7bd3269e88328845a5a91c1362f8eeb010fadc17
IzukiKa <isaacjroberts0505@gmail.com>
2022-10-30 16:20:07 +0000

    Create CraftKeeperClient.java

---
+++ src/main/java/net/izuki/craft_keeper/CraftKeeperClient.java
@@ -0,0 +1,10 @@
+package net.izuki.craft_keeper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftKeeperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
