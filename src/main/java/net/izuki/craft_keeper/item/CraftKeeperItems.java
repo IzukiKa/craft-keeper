b9fb62c45c284533a77ec06e8b0e2dfdccefc4d8
IzukiKa <isaacjroberts0505@gmail.com>
2022-10-30 16:25:21 +0000

    Create Amber Chunk

--- src/main/java/net/izuki/craft_keeper/item/CraftKeeperItems.java
+++ src/main/java/net/izuki/craft_keeper/item/CraftKeeperItems.java
@@ -12,6 +12,8 @@ public class CraftKeeperItems {
     // Register valuables
     public static final Item ADDER_STONE = registerItem("adder_stone",
             new AdderStoneItem(new FabricItemSettings().group(CraftKeeperItemGroups.VALUABLES).maxCount(999)));
+    public static final Item AMBER_CHUNK = registerItem("amber_chunk",
+            new AmberChunkItem(new FabricItemSettings().group(CraftKeeperItemGroups.VALUABLES).maxCount(999)));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(CraftKeeperMod.MOD_ID, name), item);
---
+++ src/main/java/net/izuki/craft_keeper/item/custom/AmberChunkItem.java
@@ -0,0 +1,32 @@
+package net.izuki.craft_keeper.item.custom;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class AmberChunkItem extends Item {
+    public AmberChunkItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.literal("Valuable").formatted(Formatting.WHITE));
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("Solid remains of an ancient").formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("forest. The light it reflects is").formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("warm and golden.").formatted(Formatting.GRAY));
+        } else {
+            tooltip.add(Text.literal("Press SHIFT for more info!").formatted(Formatting.YELLOW));
+        }
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
