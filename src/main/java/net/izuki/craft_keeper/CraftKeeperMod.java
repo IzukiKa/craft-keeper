2ea6af122c4c1df01a975db865e9cfd80996e44e
IzukiKa <isaacjroberts0505@gmail.com>
2022-10-30 16:17:16 +0000

    Rename ExampleMod.java
    Rename ExampleMod.java to CraftKeeperMod.java

---
+++ src/main/java/net/izuki/craft_keeper/CraftKeeperMod.java
@@ -0,0 +1,14 @@
+package net.izuki.craft_keeper;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CraftKeeperMod implements ModInitializer {
+	public static final String MOD_ID = "craft_keeper";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
